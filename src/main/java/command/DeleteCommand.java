9ddf42f64bd0c2fb953fc6b070efbc4df577bc8e
sanya <sanghavisanya@gmail.com>
2025-02-15 13:22:29 +0800

    Add index out of bounds exception handling
    - Added index out of bounds exceptions to Delete Mark, Unmark, and In progress commands
    - Exception handling in Sunpter.java
    - Removed instances of incorrect exception handling with SunpterException

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -1,5 +1,6 @@
 package command;
 
+import exception.SunpterException;
 import storage.Storage;
 import task.Roster;
 import task.Task;
@@ -9,7 +10,7 @@ import ui.Ui;
  * Removes a task from the tasklist by citing its index number
  */
 public class DeleteCommand extends Command {
-    private int num;
+    private final int num;
 
     public DeleteCommand(int num) {
         this.num = num;
@@ -17,10 +18,11 @@ public class DeleteCommand extends Command {
 
     @Override
     public String execute(Roster roster, Ui ui, Storage storage) {
-        Task task = roster.getTask(num);
-        if (task == null) {
-            return ui.taskDoesNotExist();
+        if (num < 1 || num > roster.numberofTasks()) {
+            throw new IndexOutOfBoundsException("Please input a task number between 1 and " +
+                                            (roster.numberofTasks()));
         }
+        Task task = roster.getTask(num);
         roster.removeTask(num);
         return ui.removeTaskMessage(task, roster);
     }
